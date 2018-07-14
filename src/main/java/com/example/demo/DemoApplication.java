899f15fa2ff3573ff1ab53b923187cfabbfffb5c
yang <yang092089@163.com>
2018-07-14 17:30:46 +0800

    111111111111

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 
